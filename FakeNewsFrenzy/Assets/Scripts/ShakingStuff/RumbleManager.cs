1d25929d9f3c39bcccb7c9ceaaced33e2525a0c5
Frozenpeon <thomas.verdier.sr@gmail.com>
2024-09-10 12:56:31 +0200

    [ADD] stuff

--- FakeNewsFrenzy/Assets/Scripts/ShakingStuff/RumbleManager.cs
+++ FakeNewsFrenzy/Assets/Scripts/ShakingStuff/RumbleManager.cs
@@ -6,15 +6,33 @@ using UnityEngine.InputSystem;
 
 public class RumbleManager : MonoBehaviour
 {
+    public static RumbleManager isntance;
 
-    private Coroutine RumblingCoroutine;
+    private Coroutine RumblingCoroutinePlayerOne;
+    private Coroutine RumblingCoroutinePlayerTwo;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            StartShaking(Gamepad.all[0], 1f, 1f, 1f);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StartShaking(Gamepad.all[1], 1f, 1f, 1f);
+        }
+
+    }
 
     public void StartShaking(Gamepad pad, float highFrequency, float lowFrequency, float duration)
     {
         
         pad.SetMotorSpeeds(highFrequency, lowFrequency);
 
-        RumblingCoroutine = StartCoroutine(StopRumble(duration, pad));
+        if (pad.deviceId == 0)
+            RumblingCoroutinePlayerOne = StartCoroutine(StopRumble(duration, pad));
+        else
+            RumblingCoroutinePlayerTwo = StartCoroutine(StopRumble(duration, pad));
 
     }
 
