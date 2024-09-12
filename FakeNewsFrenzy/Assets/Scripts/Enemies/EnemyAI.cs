77125aeec1c26a751abf587fea4f99987256dfed
JFautrat <j.fautrat@student.isartdigital.com>
2024-09-12 13:10:59 +0200

    [ADD] EnemyAI Attack

--- FakeNewsFrenzy/Assets/Scripts/Enemies/EnemyAI.cs
+++ FakeNewsFrenzy/Assets/Scripts/Enemies/EnemyAI.cs
@@ -9,16 +9,20 @@ public class EnemyAI : MonoBehaviour
 
     public Vector3 playerPos;
     [SerializeField] private NavMeshAgent navmeshAgent;
-
     public bool playerDetected = false;
+    [SerializeField] private GameObject meleeHitbox;
 
     public EnemySprites sprites;
+    private bool canShoot = true;
+    private bool canMove = true;
+    [SerializeField] private float distanceToAttack = 2.5f;
+    [SerializeField] private float _Speed = 3.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        navmeshAgent.speed = _Speed;
     }
 
    
@@ -26,29 +30,53 @@ public class EnemyAI : MonoBehaviour
     // Juste appelle ça quand tu veux tirer en sah
     private void Shoot()
     {
-        if (transform.GetChild(0).GetComponent<EnemyWeaponPicker>()!=null)
-            transform.GetChild(0).GetComponent<EnemyWeaponPicker>().Shoot();
-        else
+        if(canShoot == true)
         {
-            //Appeler / faire l'attaque cac 
-            if (sprites != null)
-            sprites.ShowMeleAttack();
+            if (transform.GetChild(0).GetComponent<EnemyWeaponPicker>() != null)
+                transform.GetChild(0).GetComponent<EnemyWeaponPicker>().Shoot();
+            else
+            {
+                //Appeler / faire l'attaque cac 
+                canShoot = false;
+                StartCoroutine(ShowHitbox());
+                if (sprites != null)
+                    sprites.ShowMeleAttack();
+            }
         }
+      
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (playerPos != null && playerDetected == true)
+        if (playerPos != null && playerDetected == true && canMove)
         {
             navmeshAgent.SetDestination(playerPos);
+            transform.LookAt(playerPos);
+            if (navmeshAgent.remainingDistance >= distanceToAttack)
+            {
+                navmeshAgent.isStopped = false;
+            }
+            else navmeshAgent.isStopped = true;
+
         }
-        
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetMouseButtonDown(0) || navmeshAgent.remainingDistance <= distanceToAttack)
         {
             Shoot();
         }
 
+
+
+    }
+
+    private IEnumerator ShowHitbox ()
+    {
+        meleeHitbox.SetActive(true);
+        yield return new WaitForSeconds(.7f);
+        meleeHitbox.SetActive(false);
+        yield return new WaitForSeconds(1.5f);
+        canShoot = true;
     }
 }
